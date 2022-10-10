db5037193606c5f9306b89406b44a89ca640cbac
Palloma Hortencio <palloma.hortencio@gmail.com>
2022-10-10 12:00:51 -0300

    CSS

--- src/components/layout/Cabecalho.jsx
+++ src/components/layout/Cabecalho.jsx
@@ -1,9 +1,18 @@
 import Menu from "../../Menu";
 
+/* Objeto de estilos CSS */
+const estilos = {
+  color: "white",
+  backgroundColor: "black",
+};
+
 const Cabecalho = () => {
   return (
-    <header>
-      <h1>Inerface React</h1>
+    /* CSS in JS: estilos CSS embutidos no meio do JavaScript */
+
+    /* CSS inline (usa propriedade style e sintaxe de objeto) */
+    <header style={{ backgroundColor: "lightblue", textAlign: "center]" }}>
+      <h1 style={estilos}>Inerface React</h1>
       <Menu />
     </header>
   );
