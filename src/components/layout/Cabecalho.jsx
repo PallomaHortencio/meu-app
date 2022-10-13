2f89ca3b1c6866bdb96df956b47e8d410b616457
Palloma Hortencio <palloma.hortencio@gmail.com>
2022-10-13 10:41:04 -0300

    Eventos

--- src/components/layout/Cabecalho.jsx
+++ src/components/layout/Cabecalho.jsx
@@ -1,8 +1,14 @@
 import Menu from "../../Menu";
 
 const Cabecalho = () => {
+  const exemplo2 = () => {
+    console.log("exemplo2");
+  };
   return (
     <header>
+      <button onClick={() => console.log("exemplo1")}>Exemplo 1</button>
+      <button onClick={exemplo2}>Exemplo 2</button>
+      <hr />
       <h1>Inerface React</h1>
       <Menu />
     </header>
