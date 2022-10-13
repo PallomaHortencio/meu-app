dc292b9a064e67b53187b5be2afd2c9190eb3cf6
Palloma Hortencio <palloma.hortencio@gmail.com>
2022-10-13 11:33:44 -0300

    Update Conteudo.jsx

--- src/components/layout/Conteudo.jsx
+++ src/components/layout/Conteudo.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import Artigo from "../Artigo";
 
 const Conteudo = () => {
+  const [corFundo, setCorFundo] = useState("");
+  const [contador, setContador] = useState(0);
+  const atualizaContagem = () => setContador(contador + 1);
+
+  const fundoAmarelo = () => setCorFundo("yellow");
+  const fundoAzul = () => setCorFundo("blue");
+  const fundoVermelho = () => setCorFundo("red");
+
   return (
     <>
-      <main>
-        {/* Aqui é comentario de jsx */}
+      <main style={{ backgroundColor: corFundo }}>
+        <button onMouseOver={fundoAmarelo}>Amarelo</button>
+        <button onMouseOver={fundoAzul}>Azul</button>
+        <button onMouseOver={fundoVermelho}>Vermelho</button>
+
         <section>
-          <h2>Conteúdo do site....</h2>
+          <h2 onClick={() => setContador(contador + 1)}>{contador}</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
             cumque excepturi? Consequuntur molestiae sit, blanditiis quod harum
