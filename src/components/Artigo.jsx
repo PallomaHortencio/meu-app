7209445aa254bc170681770e64c4aa0ef9003b78
Palloma Hortencio <palloma.hortencio@gmail.com>
2022-10-13 09:30:06 -0300

    Props
    Outra forma de chamar os props

--- src/components/Artigo.jsx
+++ src/components/Artigo.jsx
@@ -1,10 +1,26 @@
+/*
+prop sendo chamado manualmente
 const Artigo = (props) => {
   return (
     <article>
       <h3>{props.titulo}</h3>
       <h4>{props.subtitulo}</h4>
       <p>{props.children}</p>
-      <h4>{props.cursos}</h4>
+      <h4> Curso: {props.cursos}</h4>
+    </article>
+  );
+}; */
+
+/* Acesso ás props usando destrutcturing de objeto 
+usando as {} o programa entende que é um prop*/
+
+const Artigo = ({ titulo, subtitulo, children, cursos }) => {
+  return (
+    <article>
+      <h3>{titulo}</h3>
+      <h4>{subtitulo}</h4>
+      <p>{children}</p>
+      <h4> Curso: {cursos}</h4>
     </article>
   );
 };
